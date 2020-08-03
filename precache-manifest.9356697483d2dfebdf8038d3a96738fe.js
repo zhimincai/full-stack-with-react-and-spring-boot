48121b3788e583cce98b71380934b44e3a13ef6e
Zhimin (Jimmy) Cai <zcai@berkeley.edu>
2020-08-02 18:15:34 -0700

    Updates

---
+++ precache-manifest.9356697483d2dfebdf8038d3a96738fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3d07b4d23c5f9f84451861c67cbf18",
+    "url": "/full-stack-with-react-and-spring-boot/index.html"
+  },
+  {
+    "revision": "0e3a07522c4558dd13da",
+    "url": "/full-stack-with-react-and-spring-boot/static/css/main.8456dc75.chunk.css"
+  },
+  {
+    "revision": "66acc70b73b1696eec89",
+    "url": "/full-stack-with-react-and-spring-boot/static/js/2.fbd6b029.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/full-stack-with-react-and-spring-boot/static/js/2.fbd6b029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3a07522c4558dd13da",
+    "url": "/full-stack-with-react-and-spring-boot/static/js/main.c0c9998d.chunk.js"
+  },
+  {
+    "revision": "66bc25cfce3b2351852c",
+    "url": "/full-stack-with-react-and-spring-boot/static/js/runtime-main.3cf2cd60.js"
+  }
+]);
